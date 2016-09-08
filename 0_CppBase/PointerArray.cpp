95d62ab4e85721de446a1cea7b65bc2a7346db63
Ubuntu <ubuntu@ip-172-31-35-57.us-west-2.compute.internal>
2016-09-08 15:35:26 +0000

    array examples

--- 0_CppBase/PointerArray.cpp
+++ 0_CppBase/PointerArray.cpp
@@ -2,10 +2,46 @@
 
 using namespace std;
 
-int main(){
-	int *numbers = new int[5];
+#define BOUND 5
+
+void array(){
+	int numbers[BOUND];
+	for (int i = 0; i <= BOUND-1; i++){
+		numbers[i]=i;
+	}
+	cout << numbers[2] << endl;
+	cout << numbers[6] << endl;
+}
+
+void arrayAsPointer(){
+	int *numbers = new int[BOUND];
 	for (int i = 0; i <= 4; i++) {
 		*(numbers + i*2) = i;
 	}
-	cout << numbers[2] << endl;
+	cout << numbers[6] << endl;
+	cout << numbers[7] << endl;
+}
+
+void array2d(){
+	int a[BOUND][BOUND];
+	for (int i = 0; i < BOUND; i ++){
+		for (int j = 0; j < BOUND; j++){
+			a[i][j]=i+j;
+		}
+	}
+
+	for (int i = 0; i < BOUND; i++) {
+		for (int j = 0; j< BOUND; j++){
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}	
+}
+
+int main(){
+
+	array();
+	arrayAsPointer();
+	array2d();
+	
 }
