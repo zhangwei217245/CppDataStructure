41b26249bcb8ac248584b03a42e2b054f3b7dae6
Wei Zhang <X-Spirit.zhang@ttu.edu>
2016-11-09 15:30:02 -0600

    update AVLTree

---
+++ 7_AVLTree/AVLTree.cpp
@@ -0,0 +1,34 @@
+#include "AVLTree.h"
+using namespace std;
+void printArray(int *arr){
+   for (int i =0; i<10;i++){
+        cout << arr[i] << "  ";
+   }
+   cout << endl;
+}
+int main(){
+   AVLTree<int> AVLTree;
+   //srand(0);
+   srand(time(NULL));
+
+   int test_data[10];// declare an array to save the test data
+   for (int i = 0; i < 10; i++){
+        test_data[i] = (rand() % 100)+1; // generate the test data
+        AVLTree.root = AVLTree.insert(AVLTree.root, test_data[i]);
+   }
+   printArray(test_data); // print the test data
+   AVLTree.print_t(AVLTree.root); // print the tree
+
+   for (int i = 0; i < 10; i++){
+         // search each test data, make sure all of them will be found.
+        cout << test_data[i] << (AVLTree.search(AVLTree.root, test_data[i])?" can":" cannot")<< " be found in the tree!" << endl;
+        // search the neighbor integer of each test data, make sure all of them cannot be found.
+       cout << test_data[i]-1 << (AVLTree.search(AVLTree.root, test_data[i]-1)?" can":" cannot")<< " be found in the tree!" << endl;
+   }
+
+    cout << "Preorder:" ; AVLTree.preorder(AVLTree.root);cout << endl;
+    cout << "Inorder:" ; AVLTree.inorder(AVLTree.root); cout<< endl;
+    cout << "Postorder:"; AVLTree.postorder(AVLTree.root);cout << endl;
+
+   return 0;
+}
---
+++ 7_AVLTree/AVLTree.h
@@ -0,0 +1,250 @@
+#include <iostream>
+#include <cstddef>
+#include <cstdlib>
+#include <iomanip>
+#include <ctime>
+#include <vector>
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+template<class T>
+class TreeNode {
+public:
+    TreeNode() {
+        left = right = NULL;
+    }
+
+    TreeNode(const T &e, TreeNode<T> *l = NULL, TreeNode<T> *r = NULL) {
+        el = e;
+        left = l;
+        right = r;
+    }
+
+    T el;
+    TreeNode<T> *left, *right;
+
+};
+
+template <class T>
+class AVLTree {
+public:
+    AVLTree() {
+        root = NULL;
+    }
+
+    ~AVLTree() {
+        //Preorder traversal to delete every node.
+        //But the preorder traversal function needs to be implemented first.
+    }
+
+    int height(TreeNode<T> *subroot);
+
+    int diff(TreeNode<T> *subroot);
+
+    TreeNode<T>* rr_rotation(TreeNode<T> *subroot);
+
+    TreeNode<T>* ll_rotation(TreeNode<T> *subroot);
+
+    TreeNode<T>* lr_rotation(TreeNode<T> *subroot);
+
+    TreeNode<T>* rl_rotation(TreeNode<T> *subroot);
+
+    TreeNode<T>* balance(TreeNode<T> *subroot);
+
+    bool search(TreeNode<T> *node, const T &e);
+
+    void preorder(TreeNode<T> *tree);
+
+    void inorder(TreeNode<T> *tree);
+
+    void postorder(TreeNode<T> *tree);
+
+    TreeNode<T>* insert(TreeNode<T> *subTreeRoot, const T &e);
+
+    void print_t(TreeNode<T> *tree);
+
+    TreeNode<T> *root;
+private:
+    int _print_t(TreeNode<T> *tree, int is_left, int offset, int depth, char s[20][255]);
+};
+
+template <class T>
+TreeNode<T> *AVLTree<T>::ll_rotation(TreeNode<T> *subroot) {
+    TreeNode<T> *temp;
+    temp = subroot -> left;
+    subroot -> left = temp -> right;
+    temp -> right = subroot;
+    return temp;
+}
+template <class T>
+TreeNode<T> *AVLTree<T>::rr_rotation(TreeNode<T> *subroot) {
+    TreeNode<T> *temp;
+    temp = subroot -> right;
+    subroot -> right = temp -> left;
+    temp -> left = subroot;
+    return temp;
+}
+template <class T>
+TreeNode<T> *AVLTree<T>::lr_rotation(TreeNode<T> *subroot) {
+    TreeNode<T> *temp;
+    temp = subroot -> left;
+    subroot -> left = rr_rotation(temp);
+    return ll_rotation(subroot);
+}
+template <class T>
+TreeNode<T> *AVLTree<T>::rl_rotation(TreeNode<T> *subroot) {
+    TreeNode<T> *temp;
+    temp = subroot -> right;
+    subroot -> right = ll_rotation(temp);
+    return rr_rotation(subroot);
+}
+
+template <class T>
+TreeNode<T>* AVLTree<T>::balance(TreeNode<T> *subroot) {
+    TreeNode<T> *temp = subroot;
+    int bl_factor = diff(temp);
+    if (bl_factor > 1)
+    {
+        if (diff (temp->left) > 0)
+            temp = ll_rotation (temp);
+        else
+            temp = lr_rotation (temp);
+    }
+    else if (bl_factor < -1)
+    {
+        if (diff (temp->right) > 0)
+            temp = rl_rotation (temp);
+        else
+            temp = rr_rotation (temp);
+    }
+    return temp;
+}
+
+template <class T>
+int AVLTree<T>::diff(TreeNode<T> *subroot) {
+    return height(subroot->left) - height(subroot -> right);
+}
+
+
+template <class T>
+int AVLTree<T>::height(TreeNode<T> *subroot) {
+    if (subroot == NULL) {
+        return 0;
+    }
+    return 1 + max(height(subroot->left), height(subroot -> right));
+}
+
+template<class T>
+TreeNode<T>* AVLTree<T>::insert(TreeNode<T> *subTreeRoot, const T &e) {
+
+    if (subTreeRoot == NULL) {
+        subTreeRoot = new TreeNode<T>(e);
+    } else {
+
+        if (subTreeRoot->el > e) {
+            subTreeRoot->left = insert(subTreeRoot->left, e);
+        } else {
+            subTreeRoot->right = insert(subTreeRoot->right, e);
+        }
+        cout << "================ Before Balancing ================" << endl;
+        print_t(this->root);
+        subTreeRoot = balance(subTreeRoot);
+        cout << "================= After Balancing ================" << endl;
+        print_t(this->root);
+    }
+    return subTreeRoot;
+}
+
+template<class T>
+bool AVLTree<T>::search(TreeNode<T> *node, const T &e) {
+    bool found = false;
+    if (node == NULL) return found;
+    if (node->el == e) {
+        found = true;
+    } else {
+        found = (search(node->right, e) || search(node->left, e));
+    }
+    return found;
+}
+
+template<class T>
+void AVLTree<T>::preorder(TreeNode<T> *tree) {
+    if (tree == NULL) {
+        return;
+    }
+    cout << tree->el << " ";
+    preorder(tree->left);
+    preorder(tree->right);
+}
+
+template<class T>
+void AVLTree<T>::inorder(TreeNode<T> *tree) {
+    if (tree == NULL) {
+        return;
+    }
+    inorder(tree->left);
+    cout << tree->el << " ";
+    inorder(tree->right);
+}
+
+template<class T>
+void AVLTree<T>::postorder(TreeNode<T> *tree) {
+    if (tree == NULL) {
+        return;
+    }
+    postorder(tree->left);
+    postorder(tree->right);
+    cout << tree->el << " ";
+}
+
+
+template<class T>
+int AVLTree<T>::_print_t(TreeNode<T> *tree, int is_left, int offset, int depth, char s[20][255]) {
+    char b[20];
+    int width = 5;
+
+    if (tree == NULL) return 0;
+
+    sprintf(b, "(%03d)", tree->el);
+
+    int left = _print_t(tree->left, 1, offset, depth + 1, s);
+    int right = _print_t(tree->right, 0, offset + left + width, depth + 1, s);
+
+    for (int i = 0; i < width; i++)
+        s[2 * depth][offset + left + i] = b[i];
+
+    if (depth && is_left) {
+
+        for (int i = 0; i < width + right; i++)
+            s[2 * depth - 1][offset + left + width / 2 + i] = '-';
+
+        s[2 * depth - 1][offset + left + width / 2] = '+';
+        s[2 * depth - 1][offset + left + width + right + width / 2] = '+';
+
+    } else if (depth && !is_left) {
+
+        for (int i = 0; i < left + width; i++)
+            s[2 * depth - 1][offset - width / 2 + i] = '-';
+
+        s[2 * depth - 1][offset + left + width / 2] = '+';
+        s[2 * depth - 1][offset - width / 2 - 1] = '+';
+    }
+    return left + width + right;
+}
+
+template<class T>
+void AVLTree<T>::print_t(TreeNode<T> *tree) {
+    char s[20][255];
+    for (int i = 0; i < 20; i++)
+        sprintf(s[i], "%80s", " ");
+
+    _print_t(tree, 0, 0, 0, s);
+
+    for (int i = 0; i < 20; i++)
+        printf("%s\n", s[i]);
+}
+
+//http://stackoverflow.com/questions/13484943/print-a-binary-tree-in-a-pretty-way
+//http://stackoverflow.com/questions/801740/c-how-to-draw-a-binary-tree-to-the-console
